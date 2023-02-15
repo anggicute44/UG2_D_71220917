ba828d30e2816489d8ca8ce5423354b583792d1c
anggicute44 <117901030+anggicute44@users.noreply.github.com>
2023-02-15 18:44:49 +0700

    Add files via upload

---
+++ 1_D_71220917.py
@@ -0,0 +1,31 @@
+
+print ("=============== MAKANAN ===============")
+print ("1. Telur Bakar          : Rp 7.000")
+print ("2. Lele Terbang Mas Bhe : Rp 10.000")
+print ("3. Es Coklat Lempu      : Rp 5.000")
+print ("4.Es Doger Langensari  : Rp 13.000")
+
+print ("==================== PESANAN ====================")
+juggi = float(input("Telur Bakar                : "))
+ando = float(input("Lele Terbang          : "))
+emna = float(input("Es Coklat                  : "))
+aaai = float(input("Es Doger                   : "))
+
+print ("==================== TOTAL ====================")
+print ("TOTAL TELUT BAKAR x ",juggi,"      : ","Rp ",juggi*7000)
+print ("TOTAL LELE TERBANG x ",ando,"           : ","Rp ",ando*10000)
+print ("TOTAL ES COKELAT x ",emna,"           : ","Rp ",emna*5000)
+print ("TOTAL ES DOGER x ",aaai,"     : ","Rp ",aaai*13000)
+
+
+aman = juggi
+sssii=ando*10000
+ssshhh=juggi*7000
+namaa=aaai*13000
+buaa=emna*5000
+
+
+
+hast = ssshhh+sssii+buaa+namaa
+
+print ("Jumlah total biaya yang harus dibayar adalah Rp ",hast)
